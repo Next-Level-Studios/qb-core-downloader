bf8700bf6f348d8e6538da4408ec4c4fa9abbf44
Andrew Anderson <TheNeo2k@users.noreply.github.com>
2022-12-16 12:58:41 +0000

    updated to latest QB structure.

--- downloader/qbdownload.bat
+++ downloader/qbdownload.bat
@@ -89,7 +89,25 @@ goto downloadfiles
 
 :downloadfiles
 
+git clone -b master https://github.com/qbcore-framework/bob74_ipl ./resources/[standalone]/bob74_ipl
+git clone -b main https://github.com/qbcore-framework/safecracker ./resources/[standalone]/safecracker
+git clone -b master https://github.com/citizenfx/screenshot-basic ./resources/[standalone]/screenshot-basic
+git clone -b main https://github.com/qbcore-framework/progressbar ./resources/[standalone]/progressbar
+git clone -b master https://github.com/qbcore-framework/interact-sound ./resources/[standalone]/interact-sound
+git clone -b main https://github.com/qbcore-framework/mhacking ./resources/[standalone]/mhacking
+git clone -b master https://github.com/qbcore-framework/connectqueue ./resources/[standalone]/connectqueue
+git clone -b master https://github.com/qbcore-framework/dpemotes ./resources/[standalone]/dpemotes
+git clone -b master https://github.com/qbcore-framework/PolyZone ./resources/[standalone]/PolyZone
+git clone -b master https://github.com/qbcore-framework/LegacyFuel ./resources/[standalone]/LegacyFuel
+
+echo [1m Cooling down (we dont want to spam github)...[0m
+timeout 5 > NUL
+
+git clone -b main https://github.com/AvarianKnight/pma-voice ./resources/[voice]/pma-voice
 git clone -b main https://github.com/qbcore-framework/qb-radio ./resources/[voice]/qb-radio
+git clone -b main https://github.com/qbcore-framework/hospital_map ./resources/[defaultmaps]/hospital_map
+git clone -b main https://github.com/qbcore-framework/dealer_map ./resources/[defaultmaps]/dealer_map
+git clone -b main https://github.com/qbcore-framework/prison_map ./resources/[defaultmaps]/[prison_map]
 git clone -b main https://github.com/qbcore-framework/qb-core ./resources/[qb]/qb-core
 git clone -b main https://github.com/qbcore-framework/qb-scoreboard ./resources/[qb]/qb-scoreboard
 git clone -b main https://github.com/qbcore-framework/qb-adminmenu ./resources/[qb]/qb-adminmenu
@@ -98,13 +116,12 @@ git clone -b main https://github.com/qbcore-framework/qb-target ./resources/[qb]
 git clone -b main https://github.com/qbcore-framework/qb-customs ./resources/[qb]/qb-customs
 git clone -b main https://github.com/qbcore-framework/qb-vehiclesales ./resources/[qb]/qb-vehiclesales
 git clone -b main https://github.com/qbcore-framework/qb-vehicleshop ./resources/[qb]/qb-vehicleshop
+git clone -b main https://github.com/qbcore-framework/qb-houserobbery ./resources/[qb]/qb-houserobbery
+git clone -b main https://github.com/qbcore-framework/qb-prison ./resources/[qb]/qb-prison
 
 echo [1m Cooling down (we dont want to spam github)...[0m
 timeout 5 > NUL
 
-
-git clone -b main https://github.com/qbcore-framework/qb-houserobbery ./resources/[qb]/qb-houserobbery
-git clone -b main https://github.com/qbcore-framework/qb-prison ./resources/[qb]/qb-prison
 git clone -b main https://github.com/qbcore-framework/qb-atms ./resources/[qb]/qb-atms
 git clone -b main https://github.com/qbcore-framework/qb-hud ./resources/[qb]/qb-hud
 git clone -b main https://github.com/qbcore-framework/qb-management ./resources/[qb]/qb-management
@@ -117,16 +134,16 @@ git clone -b main https://github.com/qbcore-framework/qb-ambulancejob ./resource
 git clone -b main https://github.com/qbcore-framework/qb-vehiclefailure ./resources/[qb]/qb-vehiclefailure
 git clone -b main https://github.com/qbcore-framework/qb-radialmenu ./resources/[qb]/qb-radialmenu
 git clone -b main https://github.com/qbcore-framework/qb-crypto ./resources/[qb]/qb-crypto
-
-echo [1m Cooling down again (we dont want to spam github)...[0m
-timeout 5 > NUL
-
 git clone -b main https://github.com/qbcore-framework/qb-weathersync ./resources/[qb]/qb-weathersync
 git clone -b main https://github.com/qbcore-framework/qb-policejob ./resources/[qb]/qb-policejob
 git clone -b main https://github.com/qbcore-framework/qb-traphouse ./resources/[qb]/qb-traphouse
 git clone -b main https://github.com/qbcore-framework/qb-apartments ./resources/[qb]/qb-apartments
 git clone -b main https://github.com/qbcore-framework/qb-vehiclekeys ./resources/[qb]/qb-vehiclekeys
 git clone -b main https://github.com/qbcore-framework/qb-mechanicjob ./resources/[qb]/qb-mechanicjob
+
+echo [1m Cooling down (we dont want to spam github)...[0m
+timeout 5 > NUL
+
 git clone -b main https://github.com/qbcore-framework/qb-phone ./resources/[qb]/qb-phone
 git clone -b main https://github.com/qbcore-framework/qb-vineyard ./resources/[qb]/qb-vineyard
 git clone -b main https://github.com/qbcore-framework/qb-weapons ./resources/[qb]/qb-weapons
@@ -135,9 +152,6 @@ git clone -b main https://github.com/qbcore-framework/qb-tunerchip ./resources/[
 git clone -b main https://github.com/qbcore-framework/qb-towjob ./resources/[qb]/qb-towjob
 git clone -b main https://github.com/qbcore-framework/qb-streetraces ./resources/[qb]/qb-streetraces
 git clone -b main https://github.com/qbcore-framework/qb-storerobbery ./resources/[qb]/qb-storerobbery
-
-echo [1m Cooling down again (we dont want to spam github)...[0m
-timeout 5 > NUL
 git clone -b main https://github.com/qbcore-framework/qb-spawn ./resources/[qb]/qb-spawn
 git clone -b main https://github.com/qbcore-framework/qb-skillbar ./resources/[qb]/qb-skillbar
 git clone -b main https://github.com/qbcore-framework/qb-smallresources ./resources/[qb]/qb-smallresources
@@ -148,6 +162,10 @@ git clone -b main https://github.com/qbcore-framework/qb-truckrobbery ./resource
 git clone -b main https://github.com/qbcore-framework/qb-pawnshop ./resources/[qb]/qb-pawnshop
 git clone -b main https://github.com/qbcore-framework/qb-taxijob ./resources/[qb]/qb-taxijob
 git clone -b main https://github.com/qbcore-framework/qb-busjob ./resources/[qb]/qb-busjob
+
+echo [1m Cooling down (we dont want to spam github)...[0m
+timeout 5 > NUL
+
 git clone -b main https://github.com/qbcore-framework/qb-newsjob ./resources/[qb]/qb-newsjob
 git clone -b main https://github.com/qbcore-framework/qb-jewelery ./resources/[qb]/qb-jewelery
 git clone -b main https://github.com/qbcore-framework/qb-bankrobbery ./resources/[qb]/qb-bankrobbery
@@ -157,87 +175,101 @@ git clone -b main https://github.com/qbcore-framework/qb-fitbit ./resources/[qb]
 git clone -b main https://github.com/qbcore-framework/qb-commandbinding ./resources/[qb]/qb-commandbinding
 git clone -b main https://github.com/qbcore-framework/qb-banking ./resources/[qb]/qb-banking
 git clone -b main https://github.com/qbcore-framework/qb-clothing ./resources/[qb]/qb-clothing
-
-echo [1m Cooling down again (we dont want to spam github)...[0m
-timeout 5 > NUL
-
 git clone -b main https://github.com/qbcore-framework/qb-hotdogjob ./resources/[qb]/qb-hotdogjob
 git clone -b main https://github.com/qbcore-framework/qb-doorlock ./resources/[qb]/qb-doorlock
 git clone -b main https://github.com/qbcore-framework/qb-garbagejob ./resources/[qb]/qb-garbagejob
 git clone -b main https://github.com/qbcore-framework/qb-drugs ./resources/[qb]/qb-drugs
 git clone -b main https://github.com/qbcore-framework/qb-shops ./resources/[qb]/qb-shops
 git clone -b main https://github.com/qbcore-framework/qb-lockpick ./resources/[qb]/qb-lockpick
+
+echo [1m Cooling down (we dont want to spam github)...[0m
+timeout 5 > NUL
+
 git clone -b main https://github.com/qbcore-framework/qb-interior ./resources/[qb]/qb-interior
 git clone -b main https://github.com/qbcore-framework/qb-menu ./resources/[qb]/qb-menu
 git clone -b main https://github.com/qbcore-framework/qb-input ./resources/[qb]/qb-input
 git clone -b main https://github.com/qbcore-framework/qb-loading ./resources/[qb]/qb-loading
 
-@RD /S /Q "%CURRENTDIR%\resources\[voice]\qb-radio\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-core\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-scoreboard\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-adminmenu\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-multicharacter\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-target\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-customs\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-vehiclesales\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-vehicleshop\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-houserobbery\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-prison\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-atms\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-hud\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-management\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-weed\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-lapraces\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-inventory\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-houses\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-garages\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-ambulancejob\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-vehiclefailure\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-radialmenu\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-crypto\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-weathersync\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-policejob\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-traphouse\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-apartments\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-vehiclekeys\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-mechanicjob\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-phone\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-vineyard\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-weapons\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-scrapyard\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-tunerchip\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-towjob\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-streetraces\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-storerobbery\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-spawn\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-skillbar\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-smallresources\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-recyclejob\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-diving\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-cityhall\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-truckrobbery\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-pawnshop\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-taxijob\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-busjob\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-newsjob\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-jewelery\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-bankrobbery\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-truckerjob\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-keyminigame\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-fitbit\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-commandbinding\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-banking\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-clothing\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-hotdogjob\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-doorlock\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-garbagejob\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-drugs\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-shops\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-lockpick\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-interior\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-menu\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-input\.git"
-@RD /S /Q "%CURRENTDIR%\resources\[qb]\qb-loading\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[standalone]\bob74_ipl\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[standalone]\safecracker\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[standalone]\screenshot-basic\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[standalone]\progressbar\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[standalone]\interact-sound\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[standalone]\mhacking\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[standalone]\connectqueue\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[standalone]\dpemotes\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[standalone]\PolyZone\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[standalone]\LegacyFuel\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[voice]\pma-voice\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[voice]\qb-radio\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[defaultmaps]\hospital_map\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[defaultmaps]\dealer_map\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[defaultmaps]\[prison_map]\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-core\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-scoreboard\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-adminmenu\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-multicharacter\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-target\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-customs\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-vehiclesales\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-vehicleshop\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-houserobbery\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-prison\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-atms\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-hud\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-management\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-weed\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-lapraces\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-inventory\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-houses\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-garages\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-ambulancejob\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-vehiclefailure\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-radialmenu\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-crypto\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-weathersync\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-policejob\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-traphouse\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-apartments\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-vehiclekeys\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-mechanicjob\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-phone\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-vineyard\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-weapons\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-scrapyard\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-tunerchip\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-towjob\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-streetraces\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-storerobbery\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-spawn\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-skillbar\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-smallresources\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-recyclejob\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-diving\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-cityhall\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-truckrobbery\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-pawnshop\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-taxijob\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-busjob\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-newsjob\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-jewelery\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-bankrobbery\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-truckerjob\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-keyminigame\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-fitbit\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-commandbinding\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-banking\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-clothing\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-hotdogjob\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-doorlock\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-garbagejob\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-drugs\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-shops\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-lockpick\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-interior\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-menu\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-input\.git"
+@RD \S \Q "%CURRENTDIR%\resources\[qb]\qb-loading\.git"
 :end
 cls
 echo [102;97m Updating completed! [0m
